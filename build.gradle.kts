81844dbfe93e2bd55217b365542564c9d8f74502
Andrew-dev1 <andrewl02579@gmail.com>
2025-11-28 15:45:19 +0100

    added imports for plaid api integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,17 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt.gradle) apply false
     alias(libs.plugins.google.gms.google.services) apply false
+}
+
+buildscript {
+    repositories {
+        // Check that you have the following line (if not, add it):
+        google()  // Google's Maven repository
+        mavenCentral() // Include to import Plaid Link Android SDK
+    }
+
 }
