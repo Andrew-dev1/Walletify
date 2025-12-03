5a46b4a8d12fefe7a279dabbf46692af3b0b7e3a
Andrew-dev1 <andrewl02579@gmail.com>
2025-12-03 12:44:30 +0100

    updated Read.Me

---
+++ app/src/main/java/hu/ait/walletify/di/AuthModule.kt
@@ -0,0 +1,29 @@
+package hu.ait.walletify.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import hu.ait.walletify.data.repository.AuthRepository
+import hu.ait.walletify.data.repository.FirebaseAuthRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+}
